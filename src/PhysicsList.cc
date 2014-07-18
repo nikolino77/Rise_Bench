e9132c7e12d5bcd8240ab39fbc0a1bef6b912109
gatemaster <gatemaster@pcepcmasimu3.(none)>
2014-07-18 19:41:11 +0200

    cer

--- src/PhysicsList.cc
+++ src/PhysicsList.cc
@@ -367,7 +367,7 @@ void PhysicsList::ConstructOp()
     G4ProcessManager* pmanager = particle->GetProcessManager();
     G4String particleName = particle->GetParticleName();
     if (theCerenkovProcess->IsApplicable(*particle)) {
-      //pmanager->AddProcess(theCerenkovProcess);
+      pmanager->AddProcess(theCerenkovProcess);
       pmanager->SetProcessOrdering(theCerenkovProcess,idxPostStep);
     }
     if (theScintillationProcess->IsApplicable(*particle)) {
